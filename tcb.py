efdabd82d799d43ab6ab2b41fa23c61e2e214e1a
maria-exe <marialuizacunhadsilva@gmail.com>
2025-10-21 20:08:23 -0300

    taskControlBlock

--- tcb.py
+++ tcb.py
@@ -1,12 +1,16 @@
+
 # definição da classe task control block
 # armazena as informações das tarefas antes, durante e depois da simulação
 
 class TaskControlBlock:
-    def __init__(self, tarefa_id, cor, ingresso, duracao, prioridade):
+    # Método Construtor
+    def __init__(self, tarefa_id, cor, ingresso, duracao, prioridade, lista_eventos):
+        
         self.id = tarefa_id
         self.cor = cor
         self.ingresso = ingresso
         self.duracao = duracao 
         self.prioridade = prioridade
+        self.eventos = lista_eventos
 
 # falta: estado da tarefa, tempo de processamento já decorrido, contextos do processador
