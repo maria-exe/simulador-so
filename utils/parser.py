f86f6859e3b77fe3952730a43cb2d2cd1062da39
maria-exe <marialuizacunhadsilva@gmail.com>
2025-10-22 11:44:29 -0300

    leitura arquivo

---
+++ utils/parser.py
@@ -0,0 +1,5 @@
+
+# Função para ler as configurações de um arquivo texto (.txt)
+def ler_arquivo(caminho):
+    with open(caminho, 'r', encoding='utf-8') as config:
+       print("-.-") # continuar ..
