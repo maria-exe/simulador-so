a7ddce02be6ba12ae1561af277b57684c97769a8
maria-exe <marialuizacunhadsilva@gmail.com>
2025-10-22 21:42:37 -0300

    arquivo de configuração

--- src/config_parser.py
+++ src/config_parser.py
@@ -1,23 +1,38 @@
+from tcb import TaskControlBlock
+
 # Função para ler as configurações de um arquivo texto (.txt)
 def ler_arquivo(caminho):
    
-    with open(caminho, 'r', encoding='utf-8') as txt:
+    lista_tarefas = []
+    with open(caminho, 'r', encoding='utf-8') as txt: 
     
-    # Lê primeira linha para definir o Escalonador e o Quantum
-        p_linha = txt.readline().strip('\n')
-        p_linha = p_linha.split(';')
-        
-        algortimo = p_linha[0]
-        quantum = p_linha[1]
+        p_linha = txt.readline().strip('\n').split(';') # Lê primeira linha para definir o Escalonador e o Quantum
+        algoritimo = p_linha[0]
+        quantum = int(p_linha[1])
    
         next(txt) # Pula a primeira linha
-
-    # Loop para as configurações restantes
-        for linha in txt:
-            n_linha = linha.strip()
-            linha = n_linha.split(';')
-            print(linha)
     
-    # Falta: Salvar as informações de cada tarefa no TCB --
+        for linha in txt: # Loop para as configurações das tarefas
+            
+            parte = linha.strip().split(';')
+                 
+            tarefa_id = parte[0]
+            cor = parte[1] 
+            ingresso = int(parte[2])
+            duracao = int(parte[3])
+            prioridade = int(parte[4])
+            # eventos = parte[5] precisa ver como fazer essa parte
+        
+            nova_tarefa = TaskControlBlock(    # Instancia as tarefas
+                tarefa_id = tarefa_id,
+                cor = cor,
+                ingresso = ingresso,
+                duracao = duracao,
+                prioridade = prioridade,
+                # lista_eventos = eventos
+            )
+            
+            lista_tarefas.append(nova_tarefa)   # Adiciona na lista de tarefas
 
-ler_arquivo('conf.txt')
+    return algoritimo, quantum, lista_tarefas
+
